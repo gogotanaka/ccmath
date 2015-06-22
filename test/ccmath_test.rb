6d347472b5c956924bafb42d584b908cadd43c47
gogotanaka <mail@tanakakazuki.com>
2015-06-22 00:06:16 -0700

    Add tests cases

--- test/ccmath_test.rb
+++ test/ccmath_test.rb
@@ -17,6 +17,9 @@ class CcmathTest < Minitest::Test
     assert_in_delta 1.272019649514069+0.7861513777574233i, CCMath.sqrt(1+2i)
     assert_in_delta 3.0i, CCMath.sqrt(-9)
     assert_equal Complex(0,2), CCMath.sqrt(-4.0)
+
+
+
     assert_equal Complex(0,2), CCMath.sqrt(-4)
     assert_equal Complex(0,2), CCMath.sqrt(Rational(-4))
     assert_equal Complex(0,3), CCMath.sqrt(-9.0)
@@ -32,7 +35,11 @@ class CcmathTest < Minitest::Test
     # assert_in_delta 1.092840647090816-0.42078724841586035i, CCMath.log(-8, -2)
   end
   #
-  # def test_functions
+  def test_functions
+    assert_in_delta 1.4693517443681852+1.0634400235777521i  , CCMath.asinh(1+2i)
+    assert_in_delta 0.4270785863924755+1.5285709194809978i  , CCMath.asin(1+2i)
+    assert_in_delta 1.528570919480998+1.1437177404024204i   , CCMath.acosh(1+2i)
+    assert_in_delta 1.1437177404024204-1.528570919480998i   , CCMath.acos(1+2i)
 
 
   #   assert_in_delta 2.0327230070196656-3.0518977991517997i  , CCMath.cos(1+2i)
@@ -40,14 +47,10 @@ class CcmathTest < Minitest::Test
 
   #   assert_in_delta 1.1609640474436813+1.5972779646881088i  , CCMath.log2(1+2i)
   #   assert_in_delta 0.3494850021680094+0.480828578784234i   , CCMath.log10(1+2i)
-  #   assert_in_delta 0.4270785863924755+1.5285709194809978i  , CCMath.asin(1+2i)
-  #   assert_in_delta 1.1437177404024204-1.528570919480998i   , CCMath.acos(1+2i)
   #   assert_in_delta 1.3389725222944935+0.4023594781085251i  , CCMath.atan(1+2i)
   #   assert_in_delta 1.3389725222944935+0.4023594781085251i  , CCMath.atan2(1+2i,1)
-  #   assert_in_delta 1.4693517443681852+1.0634400235777521i  , CCMath.asinh(1+2i)
-  #   assert_in_delta 1.528570919480998+1.1437177404024204i   , CCMath.acosh(1+2i)
   #   assert_in_delta 0.17328679513998635+1.1780972450961724i , CCMath.atanh(1+2i)
-  # end
+  end
   #
   # def test_cbrt_returns_principal_value_of_cube_root
   #   assert_equal (-8)**(1.0/3), CCMath.cbrt(-8), '#3676'
