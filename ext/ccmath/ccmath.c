fbac21dae8cd20302b1622ceeb87eb7327706199
gogotanaka <mail@tanakakazuki.com>
2015-06-24 10:30:17 -0700

    Implement tanh

--- ext/ccmath/ccmath.c
+++ ext/ccmath/ccmath.c
@@ -2,11 +2,19 @@
 
 VALUE rb_mCcmath;
 
-static ID id_real_p, id_to_f;
+static ID id_real_p;
 
-static inline int basic_to_f_p(VALUE klass) {
-  return rb_method_basic_definition_p(klass, id_to_f);
-}
+#define LARGE_DBL (DBL_MAX/4.)
+
+// enum special_types {
+//     ST_NINF,            /* 0, negative infinity */
+//     ST_NEG,             /* 1, negative finite number (nonzero) */
+//     ST_NZERO,           /* 2, -0. */
+//     ST_PZERO,           /* 3, +0. */
+//     ST_POS,             /* 4, positive finite number (nonzero) */
+//     ST_PINF,            /* 5, positive infinity */
+//     ST_NAN              /* 6, Not a Number */
+// };
 
 #define RB_BIGNUM_TYPE_P(x) RB_TYPE_P((x), T_BIGNUM)
 #define BIGNUM_POSITIVE_P(b) (FIX2LONG(rb_big_cmp((b), INT2FIX(0))) >= 0)
@@ -22,222 +30,281 @@ static inline int basic_to_f_p(VALUE klass) {
 #define fix2dbl_without_to_f(x) (double) FIX2LONG(x)
 #define big2dbl_without_to_f(x) rb_big2dbl(x)
 #define int2dbl_without_to_f(x) \
-  (FIXNUM_P(x) ? fix2dbl_without_to_f(x) : big2dbl_without_to_f(x))
-#define rat2dbl_without_to_f(x)               \
-  (int2dbl_without_to_f(rb_rational_num(x)) / \
-   int2dbl_without_to_f(rb_rational_den(x)))
+    (FIXNUM_P(x) ? fix2dbl_without_to_f(x) : big2dbl_without_to_f(x))
+#define rat2dbl_without_to_f(x) \
+    (int2dbl_without_to_f(rb_rational_num(x)) / int2dbl_without_to_f(rb_rational_den(x)))
 
-static inline double num2dbl_with_to_f(VALUE num) {
-  if (SPECIAL_CONST_P(num)) {
-    if (FIXNUM_P(num)) {
-      if (basic_to_f_p(rb_cFixnum))
-        return fix2dbl_without_to_f(num);
-    } else if (FLONUM_P(num)) {
-      return RFLOAT_VALUE(num);
+static inline double num2dbl_without_to_f(VALUE num)
+{
+    if (SPECIAL_CONST_P(num)) {
+        if (FIXNUM_P(num)) {
+            return fix2dbl_without_to_f(num);
+        }
+        else if (FLONUM_P(num)) {
+            return RFLOAT_VALUE(num);
+        }
     }
-  } else {
-    switch (BUILTIN_TYPE(num)) {
-      case T_FLOAT:
-        return RFLOAT_VALUE(num);
-      case T_BIGNUM:
-        if (basic_to_f_p(rb_cBignum))
-          return big2dbl_without_to_f(num);
-        break;
-      case T_RATIONAL:
-        if (basic_to_f_p(rb_cRational))
-          return rat2dbl_without_to_f(num);
-        break;
+    else {
+        switch (BUILTIN_TYPE(num)) {
+        case T_FLOAT:
+            return RFLOAT_VALUE(num);
+        case T_BIGNUM:
+            return big2dbl_without_to_f(num);
+            break;
+        case T_RATIONAL:
+            return rat2dbl_without_to_f(num);
+            break;
+        }
     }
-  }
-  return RFLOAT_VALUE(rb_to_float(num));
+    return RFLOAT_VALUE(rb_to_float(num));
 }
 
-#define Get_Double(x) num2dbl_with_to_f(x)
+#define NUM2DBL_F(x) num2dbl_without_to_f(x)
 
-#define fun1(n) \
-  inline static VALUE f_##n(VALUE x) { return rb_funcall(x, id_##n, 0); }
+inline static VALUE f_real_p(VALUE x)
+{
+    return rb_funcall(x, id_real_p, 0);
+}
 
-fun1(real_p)
-
-    struct RComplex {
-  struct RBasic basic;
-  const VALUE real;
-  const VALUE imag;
+struct RComplex {
+    struct RBasic basic;
+    const VALUE real;
+    const VALUE imag;
 };
 
 #define RCOMPLEX(obj) (R_CAST(RComplex)(obj))
-#define RCOMPLEX_SET_REAL(cmp, r) \
-  RB_OBJ_WRITE((cmp), &((struct RComplex*)(cmp))->real, (r))
-#define RCOMPLEX_SET_IMAG(cmp, i) \
-  RB_OBJ_WRITE((cmp), &((struct RComplex*)(cmp))->imag, (i))
+#define RCOMPLEX_SET_REAL(cmp, r) RB_OBJ_WRITE((cmp), &((struct RComplex *)(cmp))->real,(r))
+#define RCOMPLEX_SET_IMAG(cmp, i) RB_OBJ_WRITE((cmp), &((struct RComplex *)(cmp))->imag,(i))
 
-inline static VALUE DBL2COMP(double real, double imag) {
-  NEWOBJ_OF(obj, struct RComplex, rb_cComplex,
-            T_COMPLEX | (RGENGC_WB_PROTECTED_COMPLEX ? FL_WB_PROTECTED : 0));
+inline static VALUE DBL2COMP(double real, double imag)
+{
+    NEWOBJ_OF(obj, struct RComplex, rb_cComplex,
+        T_COMPLEX | (RGENGC_WB_PROTECTED_COMPLEX ? FL_WB_PROTECTED : 0));
 
-  RCOMPLEX_SET_REAL(obj, DBL2NUM(real));
-  RCOMPLEX_SET_IMAG(obj, DBL2NUM(imag));
+    RCOMPLEX_SET_REAL(obj, DBL2NUM(real));
+    RCOMPLEX_SET_IMAG(obj, DBL2NUM(imag));
 
-  return (VALUE)obj;
+    return (VALUE)obj;
 }
 
-#define EXTRACT_DBL(z)                   \
-  struct RComplex* dat;                  \
-  dat = ((struct RComplex*)(z));         \
-  double z_real = Get_Double(dat->real); \
-  double z_imag = Get_Double(dat->imag);
+#define EXTRACT_DBL(z)                     \
+    struct RComplex* dat_##z;                  \
+    dat_##z = ((struct RComplex*)(z));         \
+    double z##_real = NUM2DBL_F(dat_##z->real);  \
+    double z##_imag = NUM2DBL_F(dat_##z->imag);
+
 
 #define domain_error(msg) \
-  rb_raise(rb_eMathDomainError, "Numerical argument is out of domain - " #msg)
+    rb_raise(rb_eMathDomainError, "Numerical argument is out of domain - " #msg)
 
-static VALUE ccmath_sqrt(VALUE obj, VALUE z) {
-  if (f_real_p(z)) {
-    double dz = Get_Double(z);
-    if (dz < 0.0) {
-      return DBL2COMP(0.0, sqrt(fabs(dz)));
-    } else {
-      return DBL2NUM(sqrt(Get_Double(dz)));
+static VALUE ccmath_sqrt(VALUE obj, VALUE z)
+{
+    if (f_real_p(z)) {
+        double dz = NUM2DBL_F(z);
+        if (dz < 0.0) {
+            return DBL2COMP(0.0, sqrt(fabs(dz)));
+        }
+        else {
+            return DBL2NUM(sqrt(NUM2DBL_F(dz)));
+        }
     }
-  } else {
-    EXTRACT_DBL(z)
-    double r = hypot(z_real, z_imag);
-    return DBL2COMP(sqrt((r + z_real) / 2.0), sqrt((r - z_real) / 2.0));
-  }
-}
-
-static VALUE ccmath_cos(VALUE obj, VALUE z) {
-  if (f_real_p(z))
-    return DBL2NUM(cos(Get_Double(z)));
-
-  EXTRACT_DBL(z);
-  return DBL2COMP(cos(z_real) * cosh(z_imag), -sin(z_real) * sinh(z_imag));
-}
-
-static VALUE ccmath_sin(VALUE obj, VALUE z) {
-  if (f_real_p(z))
-    return DBL2NUM(sin(Get_Double(z)));
-
-  EXTRACT_DBL(z);
-  return DBL2COMP(sin(z_real) * cosh(z_imag), cos(z_real) * sinh(z_imag));
-}
-
-static VALUE ccmath_tan(VALUE obj, VALUE z) {
-  if (f_real_p(z))
-    return DBL2NUM(tan(Get_Double(z)));
-
-  return rb_funcall(ccmath_sin(obj, z), '/', 1, ccmath_cos(obj, z));
-}
-
-static VALUE ccmath_cosh(VALUE obj, VALUE z) {
-  if (f_real_p(z))
-    return DBL2NUM(cosh(Get_Double(z)));
-
-  EXTRACT_DBL(z);
-  return DBL2COMP(cosh(z_real) * cos(z_imag), sinh(z_real) * sin(z_imag));
-}
-
-static VALUE ccmath_sinh(VALUE obj, VALUE z) {
-  if (f_real_p(z))
-    return DBL2NUM(sinh(Get_Double(z)));
-
-  EXTRACT_DBL(z);
-  return DBL2COMP(sinh(z_real) * cos(z_imag), cosh(z_real) * sin(z_imag));
-}
-
-static VALUE ccmath_tanh(VALUE obj, VALUE z) {
-  if (f_real_p(z))
-    return DBL2NUM(tan(Get_Double(z)));
-
-  return rb_funcall(ccmath_sinh(obj, z), '/', 1, ccmath_cosh(obj, z));
-}
-
-static VALUE ccmath_exp(VALUE obj, VALUE z) {
-  if (f_real_p(z))
-    return DBL2NUM(exp(Get_Double(z)));
-
-  EXTRACT_DBL(z);
-  double ere = exp(z_real);
-  return DBL2COMP(ere * cos(z_imag), ere * sin(z_imag));
-}
-
-static double internal_log(VALUE x) {
-  double d;
-  size_t numbits;
-
-  if (RB_BIGNUM_TYPE_P(x) && BIGNUM_POSITIVE_P(x) &&
-      DBL_MAX_EXP <= (numbits = rb_absint_numwords(x, 1, NULL))) {
-    numbits -= DBL_MANT_DIG;
-    x = rb_big_rshift(x, SIZET2NUM(numbits));
-  } else {
-    numbits = 0;
-  }
-
-  d = num2dbl_with_to_f(x);
-  /* check for domain error */
-  if (d < 0.0)
-    domain_error("log");
-  /* check for pole error */
-  if (d == 0.0)
-    return -INFINITY;
-
-  return log(d) + numbits * log(2); /* log(d * 2 ** numbits) */
-}
-
-static double atan2_optimized(double y, double x) {
-#ifndef M_PI
-#define M_PI 3.14159265358979323846
-#endif
-
-  if (x == 0.0 && y == 0.0) {
-    if (!signbit(x))
-      return y;
-    if (!signbit(y))
-      return M_PI;
-    return -M_PI;
-  }
-#ifndef ATAN2_INF_C99
-  if (isinf(x) && isinf(y)) {
-    /* optimization for FLONUM */
-    double z =
-        (x < 0.0) ? (3.0 * M_PI / 4.0) : (M_PI / 4.0) return (y < 0.0) ? -z : z;
-  }
-#endif
-  return atan2(y, x);
-}
-
-static VALUE ccmath_log(int argc, const VALUE* argv, VALUE obj) {
-  VALUE z, base;
-  double d;
-
-  rb_scan_args(argc, argv, "11", &z, &base);
-
-  if (f_real_p(z)) {
-    d = internal_log(z);
-    if (argc == 2) {
-      d /= internal_log(base);
+    else {
+        EXTRACT_DBL(z)
+        double s = sqrt((hypot(z_real, z_imag) + z_real) / 2.0);
+        return DBL2COMP(s, z_imag/(2 * s));
     }
-    return DBL2NUM(d);
-  } else {
+}
+
+static VALUE ccmath_cos(VALUE obj, VALUE z)
+{
+    if (f_real_p(z))
+        return DBL2NUM(cos(NUM2DBL_F(z)));
+
     EXTRACT_DBL(z);
-    float r = hypot(z_real, z_imag);
-    return DBL2COMP(log(r), atan2_optimized(z_imag, z_real));
-  }
+    return DBL2COMP(cos(z_real) * cosh(z_imag), -sin(z_real) * sinh(z_imag));
 }
 
-void Init_ccmath(void) {
-  id_real_p = rb_intern("real?");
-  id_to_f = rb_intern("to_f");
-  rb_mCcmath = rb_define_module("CCMath");
-  rb_cComplex = rb_define_class("Complex", rb_cNumeric);
-  rb_define_module_function(rb_mCcmath, "cos", ccmath_cos, 1);
-  rb_define_module_function(rb_mCcmath, "sin", ccmath_sin, 1);
-  rb_define_module_function(rb_mCcmath, "tan", ccmath_tan, 1);
-  rb_define_module_function(rb_mCcmath, "cosh", ccmath_cosh, 1);
-  rb_define_module_function(rb_mCcmath, "sinh", ccmath_sinh, 1);
-  rb_define_module_function(rb_mCcmath, "tanh", ccmath_tanh, 1);
-  rb_define_module_function(rb_mCcmath, "exp", ccmath_exp, 1);
-  rb_define_module_function(rb_mCcmath, "log", ccmath_log, -1);
-  rb_define_module_function(rb_mCcmath, "sqrt", ccmath_sqrt, 1);
+static VALUE ccmath_sin(VALUE obj, VALUE z)
+{
+    if (f_real_p(z))
+        return DBL2NUM(sin(NUM2DBL_F(z)));
 
-  // rb_define_module_function(rb_mCcmath, "sin", ccmath_sin, 1);
+    EXTRACT_DBL(z);
+    return DBL2COMP(sin(z_real) * cosh(z_imag), cos(z_real) * sinh(z_imag));
+}
+
+static VALUE ccmath_tan(VALUE obj, VALUE z)
+{
+    if (f_real_p(z))
+        return DBL2NUM(tan(NUM2DBL_F(z)));
+
+    return rb_funcall(ccmath_sin(obj, z), '/', 1, ccmath_cos(obj, z));
+}
+
+static VALUE ccmath_cosh(VALUE obj, VALUE z)
+{
+    if (f_real_p(z))
+        return DBL2NUM(cosh(NUM2DBL_F(z)));
+
+    EXTRACT_DBL(z);
+    return DBL2COMP(cosh(z_real) * cos(z_imag), sinh(z_real) * sin(z_imag));
+}
+
+static VALUE ccmath_sinh(VALUE obj, VALUE z)
+{
+    if (f_real_p(z))
+        return DBL2NUM(sinh(NUM2DBL_F(z)));
+
+    EXTRACT_DBL(z);
+    return DBL2COMP(sinh(z_real) * cos(z_imag), cosh(z_real) * sin(z_imag));
+}
+
+static VALUE ccmath_tanh(VALUE obj, VALUE z)
+{
+    if (f_real_p(z))
+        return DBL2NUM(tan(NUM2DBL_F(z)));
+
+    return rb_funcall(ccmath_sinh(obj, z), '/', 1, ccmath_cosh(obj, z));
+}
+
+static VALUE ccmath_exp(VALUE obj, VALUE z)
+{
+    if (f_real_p(z))
+        return DBL2NUM(exp(NUM2DBL_F(z)));
+
+    EXTRACT_DBL(z);
+    double ere = exp(z_real);
+    return DBL2COMP(ere * cos(z_imag), ere * sin(z_imag));
+}
+
+static double internal_log(VALUE x)
+{
+    double d;
+    size_t numbits;
+
+    if (RB_BIGNUM_TYPE_P(x) && BIGNUM_POSITIVE_P(x) && DBL_MAX_EXP <= (numbits = rb_absint_numwords(x, 1, NULL))) {
+        numbits -= DBL_MANT_DIG;
+        x = rb_big_rshift(x, SIZET2NUM(numbits));
+    }
+    else {
+        numbits = 0;
+    }
+
+    d = NUM2DBL_F(x);
+    /* check for domain error */
+    if (d < 0.0)
+        domain_error("log");
+    /* check for pole error */
+    if (d == 0.0)
+        return -INFINITY;
+
+    return log(d) + numbits * log(2); /* log(d * 2 ** numbits) */
+}
+
+static VALUE ccmath_log(int argc, const VALUE* argv, VALUE obj)
+{
+    VALUE z, base;
+    double d;
+
+    rb_scan_args(argc, argv, "11", &z, &base);
+
+    if (f_real_p(z)) {
+        d = internal_log(z);
+        if (argc == 2) {
+            d /= internal_log(base);
+        }
+        return DBL2NUM(d);
+    }
+    else {
+        EXTRACT_DBL(z);
+        float r = hypot(z_real, z_imag);
+        return DBL2COMP(log(r), atan2_optimized(z_imag, z_real));
+    }
+}
+
+static VALUE ccmath_asinh(VALUE obj, VALUE z)
+{
+    if (f_real_p(z))
+        return DBL2NUM(asinh(NUM2DBL_F(z)));
+
+    EXTRACT_DBL(z);
+    VALUE s1 = DBL2COMP(1.0+z_imag, -z_real);
+    VALUE s2 = DBL2COMP(1.0-z_imag, z_real);
+
+    s1 = ccmath_sqrt(obj, s1);
+    s2 = ccmath_sqrt(obj, s2);
+    EXTRACT_DBL(s1);
+    EXTRACT_DBL(s2);
+
+    return DBL2COMP(asinh(s1_real*s2_imag-s2_real*s1_imag), atan2(z_imag, s1_real*s2_real-s1_imag*s2_imag));
+}
+
+static VALUE ccmath_asin(VALUE obj, VALUE z)
+{
+    if (f_real_p(z))
+        return DBL2NUM(asin(NUM2DBL_F(z)));
+
+    EXTRACT_DBL(z);
+    VALUE s = ccmath_asinh(obj, DBL2COMP(-z_imag, z_real));
+    EXTRACT_DBL(s);
+    return DBL2COMP(s_imag, -s_real);
+}
+
+
+static VALUE ccmath_acosh(VALUE obj, VALUE z)
+{
+    if (f_real_p(z))
+        return DBL2NUM(acosh(NUM2DBL_F(z)));;
+
+    EXTRACT_DBL(z);
+    VALUE s1 = DBL2COMP(z_real-1.0, z_imag);
+    VALUE s2 = DBL2COMP(z_real+1.0, z_imag);
+
+    s1 = ccmath_sqrt(obj, s1);
+    s2 = ccmath_sqrt(obj, s2);
+    EXTRACT_DBL(s1);
+    EXTRACT_DBL(s2);
+
+    return DBL2COMP(asinh(s1_real*s2_real+s1_imag*s2_imag), 2.0*atan2(s1_imag, s2_real));
+}
+
+static VALUE ccmath_acos(VALUE obj, VALUE z)
+{
+    if (f_real_p(z))
+        return DBL2NUM(acos(NUM2DBL_F(z)));;
+
+    return rb_funcall(DBL2NUM(M_PI/2.0), '-', 1, ccmath_asin(obj, z));
+}
+
+// static VALUE ccmath_atanh(VALUE obj, VALUE z)
+//  log((1.0 + z) / (1.0 - z)) / 2.0
+// {
+//     if (f_real_p(z))
+//         return DBL2NUM(atanh(NUM2DBL_F(z)));;
+//
+//     EXTRACT_DBL(z);
+//     double abs_imag = fabs(z_imag)
+//     m_log1p(4.0*z_real/((1.0-z_real)*(1.0-z_real) + abs_imag*abs_imag))/4.0
+//
+//     return rb_funcall(DBL2NUM(M_PI/2.0), '-', 1, ccmath_asin(obj, z));
+// }
+
+void Init_ccmath(void)
+{
+    id_real_p = rb_intern("real?");
+    rb_mCcmath = rb_define_module("CCMath");
+    rb_cComplex = rb_define_class("Complex", rb_cNumeric);
+    rb_define_module_function(rb_mCcmath, "cos", ccmath_cos, 1);
+    rb_define_module_function(rb_mCcmath, "sin", ccmath_sin, 1);
+    rb_define_module_function(rb_mCcmath, "tan", ccmath_tan, 1);
+    rb_define_module_function(rb_mCcmath, "cosh", ccmath_cosh, 1);
+    rb_define_module_function(rb_mCcmath, "sinh", ccmath_sinh, 1);
+    rb_define_module_function(rb_mCcmath, "tanh", ccmath_tanh, 1);
+    rb_define_module_function(rb_mCcmath, "exp", ccmath_exp, 1);
+    rb_define_module_function(rb_mCcmath, "log", ccmath_log, -1);
+    rb_define_module_function(rb_mCcmath, "sqrt", ccmath_sqrt, 1);
+    rb_define_module_function(rb_mCcmath, "asinh", ccmath_asinh, 1);
+    rb_define_module_function(rb_mCcmath, "asin", ccmath_asin, 1);
+    rb_define_module_function(rb_mCcmath, "acosh", ccmath_acosh, 1);
+    rb_define_module_function(rb_mCcmath, "acos", ccmath_acos, 1);
 }
