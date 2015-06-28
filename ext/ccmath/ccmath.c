5a3c242a66cf27b67dbaf9f8ade0d16893649e21
gogotanaka <mail@tanakakazuki.com>
2015-06-28 13:15:43 -0700

    Implement lgamma, tgamma, erfc

--- ext/ccmath/ccmath.c
+++ ext/ccmath/ccmath.c
@@ -1,6 +1,7 @@
 #include "ccmath.h"
 
 VALUE rb_mCcmath;
+VALUE rb_eMathDomainError;
 
 static ID id_real_p;
 
@@ -286,10 +287,162 @@ ccmath_atan(VALUE obj, VALUE z)
     return DBL2COMP(s_imag, -s_real);
 }
 
+static VALUE
+math_erf(VALUE obj, VALUE x)
+{
+    return DBL2NUM(erf(NUM2DBL_F(x)));
+}
+
+/*
+ * call-seq:
+ *    Math.erfc(x)  -> Float
+ *
+ *  Calculates the complementary error function of x.
+ *
+ *  Domain: (-INFINITY, INFINITY)
+ *
+ *  Codomain: (0, 2)
+ *
+ *    Math.erfc(0) #=> 1.0
+ *
+ */
+
+static VALUE
+math_erfc(VALUE obj, VALUE x)
+{
+    return DBL2NUM(erfc(NUM2DBL_F(x)));
+}
+
+#ifdef __MINGW32__
+static inline double
+mingw_tgamma(const double d)
+{
+    const double g = tgamma(d);
+    return (isnan(g) && !signbit(d)) ? INFINITY : g;
+}
+#define tgamma(d) mingw_tgamma(d)
+#endif
+
+/*
+ * call-seq:
+ *    Math.gamma(x)  -> Float
+ *
+ *  Calculates the gamma function of x.
+ *
+ *  Note that gamma(n) is same as fact(n-1) for integer n > 0.
+ *  However gamma(n) returns float and can be an approximation.
+ *
+ *   def fact(n) (1..n).inject(1) {|r,i| r*i } end
+ *   1.upto(26) {|i| p [i, Math.gamma(i), fact(i-1)] }
+ *   #=> [1, 1.0, 1]
+ *   #   [2, 1.0, 1]
+ *   #   [3, 2.0, 2]
+ *   #   [4, 6.0, 6]
+ *   #   [5, 24.0, 24]
+ *   #   [6, 120.0, 120]
+ *   #   [7, 720.0, 720]
+ *   #   [8, 5040.0, 5040]
+ *   #   [9, 40320.0, 40320]
+ *   #   [10, 362880.0, 362880]
+ *   #   [11, 3628800.0, 3628800]
+ *   #   [12, 39916800.0, 39916800]
+ *   #   [13, 479001600.0, 479001600]
+ *   #   [14, 6227020800.0, 6227020800]
+ *   #   [15, 87178291200.0, 87178291200]
+ *   #   [16, 1307674368000.0, 1307674368000]
+ *   #   [17, 20922789888000.0, 20922789888000]
+ *   #   [18, 355687428096000.0, 355687428096000]
+ *   #   [19, 6.402373705728e+15, 6402373705728000]
+ *   #   [20, 1.21645100408832e+17, 121645100408832000]
+ *   #   [21, 2.43290200817664e+18, 2432902008176640000]
+ *   #   [22, 5.109094217170944e+19, 51090942171709440000]
+ *   #   [23, 1.1240007277776077e+21, 1124000727777607680000]
+ *   #   [24, 2.5852016738885062e+22, 25852016738884976640000]
+ *   #   [25, 6.204484017332391e+23, 620448401733239439360000]
+ *   #   [26, 1.5511210043330954e+25, 15511210043330985984000000]
+ *
+ */
+//
+// static VALUE
+// math_gamma(VALUE obj, VALUE x)
+// {
+//     static const double fact_table[] = {
+//         /* fact(0) */ 1.0,
+//         /* fact(1) */ 1.0,
+//         /* fact(2) */ 2.0,
+//         /* fact(3) */ 6.0,
+//         /* fact(4) */ 24.0,
+//         /* fact(5) */ 120.0,
+//         /* fact(6) */ 720.0,
+//         /* fact(7) */ 5040.0,
+//         /* fact(8) */ 40320.0,
+//         /* fact(9) */ 362880.0,
+//         /* fact(10) */ 3628800.0,
+//         /* fact(11) */ 39916800.0,
+//         /* fact(12) */ 479001600.0,
+//         /* fact(13) */ 6227020800.0,
+//         /* fact(14) */ 87178291200.0,
+//         /* fact(15) */ 1307674368000.0,
+//         /* fact(16) */ 20922789888000.0,
+//         /* fact(17) */ 355687428096000.0,
+//         /* fact(18) */ 6402373705728000.0,
+//         /* fact(19) */ 121645100408832000.0,
+//         /* fact(20) */ 2432902008176640000.0,
+//         /* fact(21) */ 51090942171709440000.0,
+//         /* fact(22) */ 1124000727777607680000.0,
+//         /* fact(23)=25852016738884976640000 needs 56bit mantissa which is
+//          * impossible to represent exactly in IEEE 754 double which have
+//          * 53bit mantissa. */
+//     };
+//     enum {NFACT_TABLE = numberof(fact_table)};
+//     double d;
+//     d = NUM2DBL_F(x);
+//     /* check for domain error */
+//     if (isinf(d) && signbit(d)) domain_error("gamma");
+//     if (d == floor(d)) {
+// 	if (d < 0.0) domain_error("gamma");
+// 	if (1.0 <= d && d <= (double)NFACT_TABLE) {
+// 	    return DBL2NUM(fact_table[(int)d - 1]);
+// 	}
+//     }
+//     return DBL2NUM(tgamma(d));
+// }
+
+/*
+ * call-seq:
+ *    Math.lgamma(x)  -> [float, -1 or 1]
+ *
+ *  Calculates the logarithmic gamma of +x+ and the sign of gamma of +x+.
+ *
+ *  Math.lgamma(x) is same as
+ *   [Math.log(Math.gamma(x).abs), Math.gamma(x) < 0 ? -1 : 1]
+ *  but avoid overflow by Math.gamma(x) for large x.
+ *
+ *    Math.lgamma(0) #=> [Infinity, 1]
+ *
+ */
+
+static VALUE
+math_lgamma(VALUE obj, VALUE x)
+{
+    double d;
+    int sign=1;
+    VALUE v;
+    d = NUM2DBL_F(x);
+    /* check for domain error */
+    if (isinf(d)) {
+	if (signbit(d)) domain_error("lgamma");
+	return rb_assoc_new(DBL2NUM(INFINITY), INT2FIX(1));
+    }
+    v = DBL2NUM(lgamma_r(d, &sign));
+    return rb_assoc_new(v, INT2FIX(sign));
+}
+
 void Init_ccmath(void)
 {
     id_real_p = rb_intern("real?");
     rb_mCcmath = rb_define_module("CCMath");
+    rb_eMathDomainError = rb_define_class_under(rb_mMath, "DomainError", rb_eStandardError);
     rb_cComplex = rb_define_class("Complex", rb_cNumeric);
     rb_define_module_function(rb_mCcmath, "cos", ccmath_cos, 1);
     rb_define_module_function(rb_mCcmath, "sin", ccmath_sin, 1);
